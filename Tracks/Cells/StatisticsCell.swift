c4a3a1b3c6861eec611755436d0f2eb07e0466d8
Nerkys <nerthard@gmail.com>
2020-07-01 13:54:28 +0300

    еще один чекпоинт

--- Tracks/Cells/StatisticsCell.swift
+++ Tracks/Cells/StatisticsCell.swift
@@ -9,37 +9,39 @@
 import UIKit
 
 class StatisticsCell: UITableViewCell, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
-    
-    
 
-//    static func nib() -> UINib {
-//        return UINib(nibName: "CollectionViewCell", bundle: nil)
-//    }
-
-    
-
+    @IBOutlet weak var collectionLayout: UICollectionViewFlowLayout! {
+        didSet {
+            collectionLayout.estimatedItemSize = UICollectionViewFlowLayout.automaticSize
+        }
+    }
     
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var collectionView: UICollectionView!
     @IBOutlet weak var view: UIView!
     @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    
+    private let itemsInSection: CGFloat = 2
+    private let itemInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: 0.0, right: 0.0)
 
     override func awakeFromNib() {
         super.awakeFromNib()
         
-//        collectionView.register(CollectionViewCell.nib(), forCellWithReuseIdentifier: "CollectionViewCell")
+        //collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+//        NSLayoutConstraint.activate([
+//            collectionView.leftAnchor.constraint(equalTo: leftAnchor),
+//            collectionView.rightAnchor.constraint(equalTo: rightAnchor),
+//            collectionView.topAnchor.constraint(equalTo: topAnchor),
+//            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+//        ])
+        
         collectionView.delegate = self
         collectionView.dataSource = self
         
-        //pageControl.hidesForSinglePage = true
-        // Initialization code
     }
-
-//    override func setSelected(_ selected: Bool, animated: Bool) {
-//        super.setSelected(selected, animated: animated)
-//
-//        // Configure the view for the selected state
-//    }
     
     override func layoutSubviews() {
         view.layer.cornerRadius = 10.0
@@ -51,6 +53,7 @@ class StatisticsCell: UITableViewCell, UICollectionViewDelegate, UICollectionVie
 
         //layer.zPosition = 1
     }
+    
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         let numberOfPages = 2
@@ -66,14 +69,45 @@ class StatisticsCell: UITableViewCell, UICollectionViewDelegate, UICollectionVie
         return cell
     }
     
-//    func collectionView
-    
-    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
         pageControl?.currentPage = Int(ceil(scrollView.contentOffset.x / scrollView.frame.width))
     }
     
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-//        return CGSize(width: 100, height: 100)
+    
+//    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+//        pageControl?.currentPage = Int(ceil(scrollView.contentOffset.x / scrollView.frame.width))
+//    }
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let availableWidth = collectionView.bounds.inset(by: collectionView.layoutMargins).width
+//        let cellWidth = (availableWidth / CGFloat(2)).rounded(.down)
+//        let paggingSpace = itemInsets.left * (itemsInSection + 1)
+//        let availableWidth = collectionView.bounds.width - paggingSpace
+//        let widthPerItem = availableWidth / itemsInSection
+//        return CGSize(width: cellWidth, height: collectionView.frame.height)
+//        return CGSize(width: collectionView.frame.width, height: collectionView.frame.height)
+        return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        itemInsets
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        if DeviceType.IS_IPHONE_5 {
+            return  -16.0
+        }
+        return 0.0
+    }
+    
+    
+    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//        return 0.0
 //    }
     
 //    func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
