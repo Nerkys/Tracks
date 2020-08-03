96d63ce5359efbbdb5528b2ea6685c6ecb4f8daa
Nerkys <nerthard@gmail.com>
2020-08-03 12:16:11 +0300

    закончил с анимациями перехода между ViewController'ами

---
+++ Tracks/TransitionManager.swift
@@ -0,0 +1,68 @@
+//
+//  TransitionManager.swift
+//  Tracks
+//
+//  Created by Mikhail Chukhvantsev on 03.08.2020.
+//  Copyright © 2020 Mikhail Chukhvantsev. All rights reserved.
+//
+
+import UIKit
+
+class TransitionManager: NSObject, UIViewControllerAnimatedTransitioning, UIViewControllerTransitioningDelegate {
+    
+    var popStyle: Bool = false
+    var presenting: Bool = true
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        0.6
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        if popStyle {
+            animatePop(using: transitionContext)
+            return
+        }
+        
+        let fromViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!
+        let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!
+
+        let finalFrame = transitionContext.finalFrame(for: toViewController)
+
+        let frameOffset = finalFrame.offsetBy(dx: finalFrame.width, dy: 0)
+        toViewController.view.frame = frameOffset
+
+        transitionContext.containerView.insertSubview(toViewController.view, aboveSubview: fromViewController.view)
+
+        UIView.animate(
+            withDuration: transitionDuration(using: transitionContext),
+            animations: {
+                fromViewController.view.frame = finalFrame.offsetBy(dx: -finalFrame.width, dy: 0)
+                toViewController.view.frame = finalFrame
+        }, completion: {_ in
+                transitionContext.completeTransition(true)
+        })
+    }
+    
+    func animatePop(using transitionContext: UIViewControllerContextTransitioning) {
+        
+        let fromViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!
+        let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!
+        
+        let finalFrame = transitionContext.initialFrame(for: fromViewController)
+        
+        let fOffPop = finalFrame.offsetBy(dx: finalFrame.width, dy: 0)
+        
+        transitionContext.containerView.insertSubview(toViewController.view, belowSubview: fromViewController.view)
+        
+        UIView.animate(
+            withDuration: transitionDuration(using: transitionContext),
+            animations: {
+                fromViewController.view.frame = fOffPop
+                fromViewController.view.alpha = 0
+        }, completion: {_ in
+                transitionContext.completeTransition(true)
+        })
+    }
+    
+}
--- Tracks/ViewController.swift
+++ Tracks/ViewController.swift
@@ -8,13 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIViewControllerTransitioningDelegate {
 
     @IBOutlet weak var seasonTableView: UITableView!
     
     var dataSource = [Season]()
     //let headerID = String(describing: SeasonHeaderView.self)
-    
+    var transitionManager = TransitionManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,8 +26,20 @@ class ViewController: UIViewController {
         //seasonTableView.reloadData()
         
         //tableViewConfig()
+        navigationController?.delegate = self
         
         self.hideNavigationBar()
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let toViewController = segue.destination as UIViewController
+//
+//        toViewController.transitioningDelegate = self.transitionManager
+//    }
+    
+    func navigationController(_ navigationController: UINavigationController, animationControllerFor operation: UINavigationController.Operation, from fromVC: UIViewController, to toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        transitionManager.popStyle = (operation == .pop)
+        return transitionManager
     }
     
 //    private func tableViewConfig() {
@@ -94,25 +106,50 @@ extension ViewController: UITableViewDataSource, UITableViewDelegate {
             let selectedTrip = self.dataSource[indexPath.section].trip[indexPath.row - 1]
             
             let theTripViewController = self.storyboard?.instantiateViewController(withIdentifier: "TheTripViewController") as! TheTripViewController
+            //theTripViewController.transitioningDelegate = transitionManager
             theTripViewController.trip = selectedTrip
             //theTripViewController.delegate = self
 //            UIView.animate(withDuration: 2.0) {() -> Void in
 //                UIView.setAnimationCurve(.easeInOut)
 //                self.navigationController?.pushViewController(theTripViewController, animated: true)
-//                //UIView.transition(with: (self.navigationController?.view)!, duration: 2, options: .beginFromCurrentState, animations: (() -> Void)?, completion: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
+//                //UIView.transition(with: (self.navigationController?.view)!, duration: 2, options: .beginFromCurrentState, animations: (() -> Void)?, completion: )
 //                UIView.setAnimationTransition(.flipFromLeft, for: (self.navigationController?.view)!, cache: false)
 //
 //            }
-            let transition = CATransition()
-            transition.duration = 5
-            transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
-            transition.type = CATransitionType.push
-            transition.subtype = CATransitionSubtype.fromRight
-            //self.view.window?.backgroundColor = .clear
-            //transition.fillMode = CAMediaTimingFillMode.forwards
-            //view.window!.layer.add(transition, forKey: kCATransition)
-            self.navigationController?.view.layer.add(transition, forKey: nil)
-            self.navigationController?.pushViewController(theTripViewController, animated: false)
+            
+//            let container = self.navigationController?.transitionCoordinator?.containerView
+//            let fromView = self.navigationController?.transitionCoordinator?.view(forKey: UITransitionContextViewKey.from)
+//            let toView = self.navigationController?.transitionCoordinator?.view(forKey: UITransitionContextViewKey.to)
+//
+//            let offScreenRight = CGAffineTransform.init(scaleX: (container?.frame.width)!, y: 0)
+//            //let offScreenLeft = CGAffineTransform.init(scaleX: -(container?.frame.width)!, y: 0)
+//
+//            container!.addSubview(toView!)
+//            container!.addSubview(fromView!)
+//
+//            UIView.animate(withDuration: 3.0, delay: 0.0, usingSpringWithDamping: 0.49, initialSpringVelocity: 0.81, options: [], animations: { () -> Void in
+//
+//                fromView!.transform = offScreenRight
+//
+//                toView!.transform = CGAffineTransform.identity
+//
+//            })
+            
+            
+            
+            
+//            let transition = CATransition()
+//            transition.duration = 5
+//            transition.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.linear)
+//            transition.type = CATransitionType.push
+//            transition.subtype = CATransitionSubtype.fromRight
+//            //self.view.window?.backgroundColor = .clear
+//            //transition.fillMode = CAMediaTimingFillMode.forwards
+//            //view.window!.layer.add(transition, forKey: kCATransition)
+//            self.navigationController?.view.layer.add(transition, forKey: nil)
+            //self.navigationController?.transitioningDelegate = transitionManager
+            self.navigationController?.pushViewController(theTripViewController, animated: true)
+            
             
         }
     }
