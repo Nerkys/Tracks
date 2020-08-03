9de6e1496c53ab78f7d1563be4eb06eb702e75e1
Nerkys <nerthard@gmail.com>
2020-08-03 15:10:40 +0300

    в процессе исправления +1/-1

--- Tracks/TheTrip/TheTripViewController.swift
+++ Tracks/TheTrip/TheTripViewController.swift
@@ -11,13 +11,21 @@ import UIKit
 class TheTripViewController: UIViewController {
     
     
+    @IBOutlet weak var theTripCollectionView: UICollectionView!
     
+    //@IBOutlet weak var view: UIView!
+    @IBOutlet weak var titleAndDateLabel: UILabel!
     @IBOutlet weak var theTripTableView: UITableView!
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var tripImage: UIImageView!
     
     @IBAction func backButton(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
     var trip: Trip!
+    private let itemsInSection: CGFloat = 3
+    private var dataSourceForColletionView = [Statistics]()
     //weak var delegate: TheTripViewController?
     //var dataSource = [Season]()
     
@@ -27,11 +35,15 @@ class TheTripViewController: UIViewController {
 //        if #available(iOS 13.0, *) {
 //            self.overrideUserInterfaceStyle = .light
 //        }
-        //dataSource = getDataSourceInitialValue()
+        
+        dataSourceForColletionView = trip.statistics
+        
         theTripTableView.delegate = self
         theTripTableView.dataSource = self
         
         theTripTableView.rowHeight = UITableView.automaticDimension
+        
+        titleAndDateLabel.text = trip.title
         
         self.hideNavigationBar()
     }
@@ -40,27 +52,17 @@ class TheTripViewController: UIViewController {
 extension TheTripViewController: UITableViewDataSource, UITableViewDelegate {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return trip.resorts.count + 1
+        return trip.resorts.count
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if section == 0 {
-            return 1
-        } else {
-            return trip.resorts[section - 1].days.count
-        }
+            return trip.resorts[section].days.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "TheTripStatisticsCell", for: indexPath) as! TheTripStatisticsCell
-            
-            cell.configure(with: trip)
 
-            return cell
-        } else {
             let cell = tableView.dequeueReusableCell(withIdentifier: "ResortsDayCell", for: indexPath) as! ResortsDayCell
-            let day = trip.resorts[indexPath.section - 1].days[indexPath.row]
+            let day = trip.resorts[indexPath.section].days[indexPath.row]
             
             cell.titleLabel.text = day.date
             cell.maxSpeedLabel.text = "\(String(day.maxSpeed)) км/ч"
@@ -69,44 +71,100 @@ extension TheTripViewController: UITableViewDataSource, UITableViewDelegate {
             cell.selectionStyle = .none
             
             return cell
-        }
         
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        if  section == 0 {
-            return nil
-        } else {
+
             let header = tableView.dequeueReusableCell(withIdentifier: "TheResortHeaderCell") as! TheResortHeaderCell
 
-            header.titleLabel.text = trip.resorts[section - 1].title
+            header.titleLabel.text = trip.resorts[section].title
 
             return header
-        }
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
-        if section == 0 {
-            return 0
-        } else {
             return 30
-        }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //guard let selectedTripCell = tableView.cellForRow(at: indexPath) as? TripCell else { return }
         
-        if indexPath.section != 0 {
-            let selectedDay = trip.resorts[indexPath.section - 1].days[indexPath.row]
-            
-            let theDayViewController = self.storyboard?.instantiateViewController(withIdentifier: "TheDayViewController") as! TheDayViewController
-            
-            theDayViewController.setDay(day: selectedDay)
-            //theTripViewController.delegate = self
-            
-            self.navigationController?.pushViewController(theDayViewController, animated: true)
-        }
+        let selectedDay = trip.resorts[indexPath.section].days[indexPath.row]
+        
+        let theDayViewController = self.storyboard?.instantiateViewController(withIdentifier: "TheDayViewController") as! TheDayViewController
+        
+        theDayViewController.setDay(day: selectedDay)
+        //theTripViewController.delegate = self
+        
+        self.navigationController?.pushViewController(theDayViewController, animated: true)
     }
     
 }
 
+extension TheTripViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+            return dataSourceForColletionView.count / Int(itemsInSection)
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+            return Int(itemsInSection)
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TheTripCollectionViewCell", for: indexPath) as! TheTripCollectionViewCell
+            let statistics = dataSourceForColletionView[indexPath.item + ((indexPath.section) * Int(itemsInSection))]
+            
+            cell.titleLabel.text = statistics.title
+            cell.actionImage.image = UIImage(named: statistics.image)
+            
+            switch statistics.title {
+            case "макс. скорость":
+                cell.valueLabel?.text = "\(String(statistics.value)) км/ч"
+            case "расстояние":
+                cell.valueLabel?.text = "\(String(statistics.value)) м"
+            case "на горе":
+                cell.valueLabel?.text = "\(String(statistics.value)) мин"
+            case "время спуска":
+                cell.valueLabel?.text = "\(String(statistics.value)) мин"
+            default:
+                cell.valueLabel?.text = String(statistics.value)
+            }
+            
+            return cell
+        }
+        
+        func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+            pageControl?.currentPage = Int(ceil(theTripCollectionView.contentOffset.x / theTripCollectionView.frame.width))
+            if pageControl?.currentPage == 0 {
+                titleAndDateLabel.text = trip.title
+            } else {
+                titleAndDateLabel.text = trip.startedAt
+            }
+            
+        }
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+    //        let paggingSpace = sectionInsets.left * (itemsInSection + 1)
+    //        let availableWidth = tripCollectionView.frame.width - paggingSpace
+            let widthPerItem = theTripCollectionView.frame.width / itemsInSection
+            return CGSize(width: widthPerItem, height: theTripCollectionView.frame.height)
+        }
+        
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    //        sectionInsets
+    //    }
+        
+        
+        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+            return 0.0
+        }
+        
+    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    //        return 0.0
+    //    }
+    
+    
+    
+}
